818e7ac1df4150d56cccb41050e45ee03bb6d7a1
Exolios <thijss@live.nl>
2015-10-06 16:17:23 +0200

    Splash screen

---
+++ app/src/main/java/group6/kb_50/marketaid/SplashActivity.java
@@ -0,0 +1,46 @@
+package group6.kb_50.marketaid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by Thijs on 06/10/2015.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_scherm);
+
+        final ImageView ivv = (ImageView) findViewById(R.id.imageView);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_slide_out_top);
+
+        ivv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                ivv.startAnimation(an2);
+                finish();
+                Intent i = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
+
