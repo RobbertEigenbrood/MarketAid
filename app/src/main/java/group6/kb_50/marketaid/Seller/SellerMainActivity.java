5a2ba5ed2260a64faf170dbef25627eb2bbecf1e
Exolios <thijss@live.nl>
2015-10-07 15:47:36 +0200

    Menu toegevoeged

--- app/src/main/java/group6/kb_50/marketaid/Seller/SellerMainActivity.java
+++ app/src/main/java/group6/kb_50/marketaid/Seller/SellerMainActivity.java
@@ -1,12 +1,18 @@
 package group6.kb_50.marketaid.Seller;
 
-import android.content.Context;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
-/*import android.view.Menu;
-import android.view.MenuItem;*/
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -15,11 +21,13 @@ import android.widget.Toast;
 
 import group6.kb_50.marketaid.R;
 
-public class SellerMainActivity extends AppCompatActivity {
+public class SellerMainActivity extends AppCompatActivity
+        implements SellerNavigationFragment.NavigationDrawerCallbacks {
+
     //---the images to display---
     Integer[] imageIDs = {
-      /*      R.mipmap.pic1,
-            R.mipmap.pic2,
+            R.mipmap.faallogoo,
+      /*      R.mipmap.pic2,
             R.mipmap.pic3,
             R.mipmap.pic4,
             R.mipmap.pic5,
@@ -27,10 +35,29 @@ public class SellerMainActivity extends AppCompatActivity {
             R.mipmap.pic7*/
     };
 
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private SellerNavigationFragment mSellerNavigationFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_main);
+
+        mSellerNavigationFragment = (SellerNavigationFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer1);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mSellerNavigationFragment.setUp(
+                R.id.navigation_drawer1,
+                (DrawerLayout) findViewById(R.id.drawer_layout1));
 
         GridView gridView = (GridView) findViewById(R.id.GridViewSeller);
         gridView.setAdapter(new ImageAdapter(this));
@@ -44,6 +71,65 @@ public class SellerMainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mSellerNavigationFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.seller_main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class ImageAdapter extends BaseAdapter {
@@ -89,36 +175,44 @@ public class SellerMainActivity extends AppCompatActivity {
 
 
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
 
-
-
-
-
-
-
-
-
-
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_seller_overview, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
         }
 
-        return super.onOptionsItemSelected(item);
-    }*/
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_seller_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((SellerMainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
 }
